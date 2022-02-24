39bfeb397c8b2fd95dced127feb0e04dae0a00e7
quentinaslan <quentin.aslan@outlook.com>
2022-02-24 15:43:16 +0100

    Add LoRa_gateway folder

---
+++ LoRa_gateway/LoRa_gateway.ino
@@ -0,0 +1,5 @@
+/**
+* La gateway devra recevoir les messages et les renvoyer au sensor community project.
+* Ce serait bien de mettre en place le protocole LoraWAN pour chiffer les échanges entre les capteurs et la gateway
+* Un exemple de code duplex est dispo dans LoRa/
+*/
