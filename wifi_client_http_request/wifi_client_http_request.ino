1ad7047759d46507af129aa56080e4b50d386aa3
quentinaslan <quentin.aslan@outlook.com>
2022-03-03 16:55:57 +0100

    Update wifi client http request (improvement)

--- wifi_client_http_request/wifi_client_http_request.ino
+++ wifi_client_http_request/wifi_client_http_request.ino
@@ -2,22 +2,26 @@
 #include <HTTPClient.h>
 
 // CREDENTIAL WIFI !! (DONT FORGET)
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "Mi 11 Lite";
+const char* password = "quentin1";
 
 // ENDPOINTS
-const char* endpoint = "http://192.168.1.50:31830/dht22";
+const char* endpoint = "http://89.82.200.242:31830/test";
+int timerDelay = 10000; // Toutes les 10 secondes on rentre dans la boucle
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     // CONNECTION AU REASEAU WIFI
     WiFi.begin(ssid, password);
     Serial.println("Connecting");
-    while (WiFi.status() != WL_CONNECTED) { // ON NE CONTINUE PAS TANT QUE L'ESP EST PAS CONNECTÉ A LA WIFI, on essaye toutes les 500ms
+
+    // Tant que l'ESP n'est pas connecté au WIFI, on boucle, toutes les 500ms ...
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
+
     Serial.println("");
     Serial.print("Connected to WiFi network with IP Address: ");
     Serial.println(WiFi.localIP());
@@ -27,24 +31,23 @@ void loop() {
     //Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED) {
         Serial.println("On est connecté à la wifi");
-        sendHTTPRequestPOST("I love my job");
+
+        // Avec des variables =>
+        // char datas[150]
+        // Exemple : sprintf(datas, "{\"humidity\":\"%f\",\"temperature\":\"%f\",\"realFeel\":\"%f\",\"location\":\"%s\"}", humidityFloat, temperatureFloat, realFeelFloat, DHT22_LOCATION);
+        sendHTTPRequestPOST("\"msg\": \"I love my job\"");
     } else {
         Serial.println("WiFi Disconnected");
     }
     delay(timerDelay);
 }
 
-void sendHTTPRequestPOST(payload) {
+// Envoie une requête HTTP.
+void sendHTTPRequestPOST(String payload) {
     HTTPClient http;
-
-    // ALL
-    char datas[150];
-    sprintf(datas, payload);
-    // Exemple : sprintf(datas, "{\"humidity\":\"%f\",\"temperature\":\"%f\",\"realFeel\":\"%f\",\"location\":\"%s\"}", humidityFloat, temperatureFloat, realFeelFloat, DHT22_LOCATION);
-
     http.begin(endpoint);
     http.addHeader("Content-Type", "application/json");
-    int datasStatus = http.POST(datas);
+    int datasStatus = http.POST(payload);
     Serial.print("HTTP Response code : ");
     Serial.println(datasStatus);
     http.end();
