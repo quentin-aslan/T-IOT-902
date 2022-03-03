8cfc82c95cb416178bd263d261611b8d46c41f00
quentinaslan <quentin.aslan@outlook.com>
2022-03-03 17:11:44 +0100

    wifi access point work :)

--- wifi_access_point/wifi_access_point.ino
+++ wifi_access_point/wifi_access_point.ino
@@ -1,9 +1,18 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-void setup() {
+const char* ssid     = "I love my job";
+const char* password = "Skribbl.io";
 
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("\n[*] Creating AP ...");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.print("[+] AP Created with IP Gateway ");
+    Serial.println(WiFi.softAPIP());
 }
 
-void loop() {
+void loop(){
 
 }
