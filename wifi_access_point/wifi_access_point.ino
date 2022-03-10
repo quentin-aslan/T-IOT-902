540360e9beec3b73225706f3491d2f3208f29c88
quentinaslan <quentin.aslan@outlook.com>
2022-03-10 10:46:23 +0100

    Create access point if wifi connect failed

--- wifi_access_point/wifi_access_point.ino
+++ wifi_access_point/wifi_access_point.ino
@@ -1,11 +1,39 @@
 #include <WiFi.h>
 
-const char* ssid     = "I love my job";
-const char* password = "Skribbl.io";
+// CREDENTIAL WIFI !! (DONT FORGET)
+const char* ssid = "Mi 11 Lite";
+const char* password = "quentin1";
 
-void setup()
-{
+// CREDENTIAL AP
+const char* ssid_AP     = "I love my job";
+const char* password_AP = "Skribbl.io";
+
+void setup() {
     Serial.begin(9600);
+
+    // CONNECTION AU REASEAU WIFI
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to the WIFI ...");
+
+    // Si au bout de 20 secondes la connexion au WIFI échoue, on créer l'access point
+    int timer = 0;
+    while ((WiFi.status() != WL_CONNECTED) && timer != 20) {
+        delay(500);
+        Serial.print(".");
+        timer = timer + 1;
+    }
+
+    if(WiFi.status() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.print("Connected to WiFi network with IP Address: ");
+        Serial.println(WiFi.localIP());
+    } else {
+      Serial.println("Connection à la wifi echoué, creation d'un access point");
+        startWifiAccessPoint();
+    }
+}
+
+void startWifiAccessPoint() {
     Serial.println("\n[*] Creating AP ...");
     WiFi.mode(WIFI_AP);
     WiFi.softAP(ssid, password);
