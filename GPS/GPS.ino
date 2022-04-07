759dc59a6c99f59923398dfaa7e85e17de02f1d3
quentinaslan <quentin.aslan@outlook.com>
2022-04-07 17:13:08 +0200

    Add GPS module + refactoring in simple function.

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,30 +1,73 @@
 #include <M5Stack.h>
+#include <TinyGPS++.h>
 
-/* By default, GPS is connected with M5Core through UART2 */
-HardwareSerial GPSRaw(2);
+
+/**
+ * INIT GPS
+ */
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps; // Librairie GPS.
+HardwareSerial ss(2); // Connexion série au GPS.
+
+/**
+ * Récupère les valeurs du module GPS et les affiches dans la Serial.
+ */
+void getGPSDatas() {
+    Serial.println("--------------------------------");
+
+    Serial.println("nombre de satellites contacté : ");
+    Serial.println(gps.satellites.value());
+
+    Serial.println("Longitude (lng) : ");
+    Serial.println(gps.location.lng());
+
+    Serial.println("Latitude (lat) : ");
+    Serial.println(gps.location.lat());
+
+    Serial.println("Dernière trames reçu des GPS (ms) : ");
+    Serial.println(gps.location.age());
+
+    Serial.println("gps.altitude.isValid() : ");
+    Serial.println(gps.altitude.isValid());
+
+    Serial.println("gps.altitude.meters() : ");
+    Serial.println(gps.altitude.meters());
+
+    Serial.println("--------------------------------");
+
+    if (millis() > 5000 && gps.charsProcessed() < 10) {
+        // LE GPS EST HORS SERVICES !!!
+        Serial.println("Aucune datas GPS reçu !");
+    }
+}
+
+/**
+ * Ajoute un delay et s'assure que le module GPS est bien présent.
+ * @param ms
+ */
+static void GPSsmartDelay(unsigned long ms) {
+    unsigned long start = millis();
+    do {
+        while (ss.available()) {
+            gps.encode(ss.read());
+        }
+    } while (millis() - start < ms);
+}
 
 void setup() {
-
     M5.begin();
-    GPSRaw.begin(9600);// GPS init
+    M5.Power.begin();
+    ss.begin(GPSBaud);
 
     Serial.begin(9600);
-
-    Serial.println("hello");
-    //termInit();
+    Serial.println("SETUP FINISH");
 }
 
 void loop() {
-  Serial.println("----------------------------");
-    // put your main code here, to run repeatedly:
-    if(Serial.available()) {
-        int ch = Serial.read();
-        GPSRaw.write(ch);
-    }
+    M5.Lcd.setCursor(0, 70);
+    M5.Lcd.setTextColor(WHITE, BLACK);
 
-    if(GPSRaw.available()) {
-        int ch = GPSRaw.read();// read GPS information
-        Serial.write(ch);
-        //termPutchar(ch);
-    }
+
+    getGPSDatas();
+    GPSsmartDelay(1000);
 }
